52a71ff26c1fff569d28ea1885b83a481b4d0392
shaikha <shaikha@Shaikha-Hp.localdomain>
2024-02-08 15:15:11 +0300

    created seller schema in models

---
+++ models/seller.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const sellerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email:{ 
+    type: String,
+    required:true,
+  },
+  Rating:{
+    type: Number,
+    enum:['0','1','2','3','4','5']
+  },
+  reviews:[reviewSchema],
+  auctions:[auctionSchema]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Seller', sellerSchema);
+
+
+
+
+
+
