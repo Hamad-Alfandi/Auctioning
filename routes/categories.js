6824bb53d1b650730c47f423a47c08d0e2f56303
Hamad-Alfandi <Hamad.alfandi7@gmail.com>
2024-02-12 15:38:43 +0300

    added categories with products related to it

--- routes/categories.js
+++ routes/categories.js
@@ -1,19 +1,42 @@
 var express = require("express")
 var router = express.Router()
-const Product = require('../models/product')
-const  Category = require('../models/auction')
-const categoriesCtrl = require("../controllers/auctioning")
+const Product = require("../models/product")
+const Category = require("../models/auction")
+
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-
-
   const categories = await Product.find({})
 
   console.log(categories)
-  res.render("auctioning/categories", { title: "All Categories" , categories})
-
-
+  res.render("categories/show", { title: "All Categories", categories })
 })
+
+// GET request for a specific category
+// GET request for a specific category or all products if no category specified
+router.get("/:category", async function (req, res, next) {
+  const encodedCategory = req.params.category;
+  
+  const category = await Category.find({category : req.params.category})
+
+  // console.log("bb ",category)
+
+
+  if (encodedCategory) {
+    const decodedCategory = decodeURIComponent(encodedCategory);
+
+    // Assuming your Product model has a field called 'category'
+    const categoryProducts = await Product.find({ category: decodedCategory });
+
+    console.log(categoryProducts);
+    res.render(`categories/category`, { title: decodedCategory, categoryProducts, category });
+  } else {
+    // Fetch all products if no category specified
+    const allProducts = await Product.find({});
+    console.log(allProducts);
+    res.render("auctioning/products", { title: "All Products", products: allProducts });
+  }
+});
+
 
 module.exports = router
