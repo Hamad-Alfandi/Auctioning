95e8ec6147590085b841e913cebc2567ce5360a1
shaikha <shaikha@Shaikha-Hp.localdomain>
2024-02-12 18:33:46 +0300

    if statement AAU buyer or seller they can view their account

--- models/review.js
+++ models/review.js
@@ -1,31 +1,36 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const reviewSchema = new Schema(
   {
     reviewRate: {
       type: Number,
-      required: true
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     seller_id: {
       type: Schema.Types.ObjectId,
-      ref: 'Seller',
-      required: true
+      ref: "Seller",
+      required: true,
     },
     buyer_id: {
       type: Schema.Types.ObjectId,
-      ref: 'Buyer',
-      required: true
+      ref: "Buyer",
+      required: true,
     },
     timeCreated: Date,
     description: {
       //text description from buyer as review
-      type: String
-    }
+      type: String,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 )
 
-module.exports = mongoose.model('Review', reviewSchema)
+module.exports = mongoose.model("Review", reviewSchema)
