48629c715734d1b15a64d31a95cb02150e7bf0e8
shaikha <jshaikha0@gmail.com>
2024-02-14 14:10:50 +0300

    models

--- models/auction.js
+++ models/auction.js
@@ -1,82 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const auctionSchema = new Schema(
   {
     seller_id: {
       type: Schema.Types.ObjectId,
-      ref: 'Seller',
-      required: true
+      ref: "Seller",
+      required: true,
     },
     endDate: {
       type: Date,
-      required: true
+      required: true,
     },
     startingBid: {
       type: Number,
-      required: true
+      required: true,
     },
     highestBid: Number,
     buyer_id: {
       type: Schema.Types.ObjectId,
-      ref: 'Buyer'
+      ref: "Buyer",
     },
     category: {
       type: String,
       required: true,
       enum: [
-        'Home Living',
-        'Clothing',
-        'Bags',
-        'Jewelry',
-        'Beauty',
-        'Collectibles',
-        'Art',
-        'Books',
-        'Other'
+        "Home Living",
+        "Clothing",
+        "Bags",
+        "Jewelry",
+        "Beauty",
+        "Collectibles",
+        "Art",
+        "Books",
+        "Other",
       ],
-      
-      default: 'Other'
-    }, 
-    auction: {
-      seller_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Seller',
-        required: true
-      },
-      endDate: {
-        type: Date,
-        required: true
-      },
-      startingBid: {
-        type: Number,
-        required: true
-      },
-      highestBid: Number,
-      buyer_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Buyer'
-      },
-      category: {
-        type: String,
-        required: true,
-        enum: [
-          'Home Living',
-          'Clothing',
-          'Bags',
-          'Jewelry',
-          'Beauty',
-          'Collectibles',
-          'Art',
-          'Books',
-          'Other'
-        ],
-        default: 'Other'
-      }
-    }
-  },
+
+      default: "Other",
+    },
+  
   {
-    timestamps: true
+    timestamps: true,
   }
 )
-module.exports = mongoose.model('Auction', auctionSchema)
+module.exports = mongoose.model("Auction", auctionSchema)
